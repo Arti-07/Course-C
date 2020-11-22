8553b44e46acff7059746fd2ffb818a6fa5acc64
Artur Gin <frank.tr@mail.ru>
2020-11-22 19:22:20 +0300

    Ypr3

--- Task1.1/Task1.1/Program.cs
+++ Task1.1/Task1.1/Program.cs
@@ -23,48 +23,48 @@ namespace Task1._1
             return true;
 
             static void Main(string[] args)
-        {
-            //
-            // 1 задание
-            //
-            Console.Write("\nПривет! Как тебя зовут?  ");
-            string name = Console.ReadLine();
-            Console.WriteLine("\nДоброго времени суток, " + name);
-            //
-            //
-            //
-            //     2 задание
-            //    
+            {
+                //
+                // 1 задание
+                //
+                Console.Write("\nПривет! Как тебя зовут?  ");
+                string name = Console.ReadLine();
+                Console.WriteLine("\nДоброго времени суток, " + name);
+                //
+                //
+                //
+                //     2 задание
+                //    
 
-            Console.WriteLine("\nПривет! Введи два числа.");
-            Console.Write("\n 1st = ");
-            string str = Console.ReadLine();
-            int k;
-            bool chislo1 = int.TryParse(str, out k);
-            if (chislo1) { }
-            else
-            {
-                Console.WriteLine("\nНекорректное значение!");
-                return;
-            }
-            Console.Write("\n 2nd = ");
-            string str2 = Console.ReadLine();
-            int l;
-            bool chislo2 = int.TryParse(str2, out l);
-            if (chislo2) { }
-            else
-            {
-                Console.WriteLine("\nНекорректное значение!");
-                return;
-            }
-            if (l != 0)
-            {
-                double n = k * 1.0 / l;
-                Console.WriteLine("\nРезультат деления первого числа на второе =  " + n);
-                Console.ReadLine();
-            }
-            else
-                Console.WriteLine("ОШИБКА! ДЕЛЕНИЕ НА 0 ");
+                Console.WriteLine("\nПривет! Введи два числа.");
+                Console.Write("\n 1st = ");
+                string str = Console.ReadLine();
+                int k;
+                bool chislo1 = int.TryParse(str, out k);
+                if (chislo1) { }
+                else
+                {
+                    Console.WriteLine("\nНекорректное значение!");
+                    return;
+                }
+                Console.Write("\n 2nd = ");
+                string str2 = Console.ReadLine();
+                int l;
+                bool chislo2 = int.TryParse(str2, out l);
+                if (chislo2) { }
+                else
+                {
+                    Console.WriteLine("\nНекорректное значение!");
+                    return;
+                }
+                if (l != 0)
+                {
+                    double n = k * 1.0 / l;
+                    Console.WriteLine("\nРезультат деления первого числа на второе =  " + n);
+                    Console.ReadLine();
+                }
+                else
+                    Console.WriteLine("ОШИБКА! ДЕЛЕНИЕ НА 0 ");
 
                 //
                 //  4 заданиe
@@ -84,5 +84,6 @@ namespace Task1._1
                 Console.WriteLine($"x1={x1}, x2={x2}");
 
             }
+        }
     }
 }
---
+++ Упр3/Упр3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Упр3
+{
+    class Program
+    {
+        public enum vyz { КГУ, КАИ, КХТИ, КФУ, МГУ }
+        struct rabotnik
+        {
+            public string name;
+            public string vus;
+            public string secondname;
+            public int zp;
+        }
+
+        static void Main(string[] args)
+        {
+            vyz novi = vyz.КФУ;
+            rabotnik kiko;
+            kiko.name = "Володя";
+            kiko.secondname = "Ибрагимов";
+            kiko.vus = Convert.ToString(novi);
+            kiko.zp = 1000;
+            Console.WriteLine(kiko.name);
+            Console.WriteLine(kiko.vus);
+            Console.WriteLine(kiko.zp);
+            Console.WriteLine(kiko.secondname);
+            Console.ReadLine();
+
+        }
+    }
+}
