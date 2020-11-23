db517ae8ccee5025fe4bf4b52831601156d35490
Artur Gin <frank.tr@mail.ru>
2020-11-23 21:22:38 +0300

    n1

---
+++ Task1.1/Следующий символ/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Следующий_символ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                ConsoleKey key = Console.ReadKey().Key;
+                int keykode = (int)key;
+                Console.WriteLine();
+                Console.WriteLine("Следующий символ " + key + 1);
+            }
+        }
+    }
+}
