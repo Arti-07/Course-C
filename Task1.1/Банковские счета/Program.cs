a09c8398bfa42a5936b33b4c893beb6f1ed44ca2
Artur Gin <frank.tr@mail.ru>
2020-11-23 21:38:14 +0300

    bank

---
+++ Task1.1/Банковские счета/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Банковские_счета
+{
+    class Program
+    {
+        public enum schet {Текущий, Кредитный, Депозитный, Бюджетный, Для_соц_выплат}
+       
+        static void Main(string[] args)
+        {
+            schet MyShet = new schet();
+            MyShet = schet.Депозитный;
+            Console.WriteLine(MyShet);
+            Console.ReadLine();
+        }
+    }
+}
